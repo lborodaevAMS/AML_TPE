e04cfbd4cd19cb46bd37630bb8624e3824ad899d
leonidborodaev <sugarvio@gmail.com>
2020-12-21 12:21:55 +0100

    validation performance

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from sklearn.svm import SVC, LinearSVC
 from sklearn.decomposition import PCA
 from sklearn.utils.testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
+from sklearn.pipeline import Pipeline
 import os
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from functools import partial
+import typing
 
 
 pd.options.display.max_rows = 999
@@ -44,16 +46,8 @@ space = hp.choice('classifier_type', [
 ])
 
 
-
-@ignore_warnings(category=ConvergenceWarning)
-def objective(args, data):
-    X_train, X_test, y_train, y_test = data['X_train'], data['X_test'], data['y_train'], data['y_test']
+def __instantiate_classifier(args):
     preprocess = args['preprocessing']
-    if preprocess != 'None':
-        pca = PCA(n_components=preprocess, svd_solver='full')
-        pca.fit(X_train)
-        X_train_pc = pca.transform(X_train)
-        X_test_pc = pca.transform(X_test)
     del args['preprocessing']
     classifier_type = args['type']
     del args['type']
@@ -67,25 +61,45 @@ def objective(args, data):
     else:
         raise NotImplemented
     if preprocess != 'None':
-        clf.fit(X_train_pc, y_train)
-        y_pred = clf.predict(X_test_pc)
+        pca = PCA(n_components=preprocess, svd_solver='full')
+        pipe = Pipeline([('pca', pca), ('clf', clf)])
     else:
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
+        pipe = Pipeline([('clf', clf)])
+    return pipe
+
+
+@ignore_warnings(category=ConvergenceWarning)
+def objective(args, data):
+    X_train, X_test, y_train, y_test = train_test_split(data['X'], data['y'])
+    clf = __instantiate_classifier(args)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    err = (y_pred != y_test).mean()
     del clf
-    return (y_pred != y_test).mean()
+    return err
 
 
-def plot_losses(losses):
+def _plot_best_so_far_loss(losses):
     best_so_far = np.inf
-    x = np.full(len(losses) // 5, np.inf)
+    x = []
+    y = []
     for i in range(len(losses)):
         if losses[i] < best_so_far:
             best_so_far = losses[i]
-        if i % 5 == 0:
-            x[i // 5] = best_so_far
-    plt.plot(range(len(x)), x, marker='x')
+            x.append(i)
+            y.append(best_so_far)
+    plt.plot(x, y, marker='x')
     plt.show()
+
+
+def _load_dataset(path_prefix: str) -> typing.Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    X_train = pickle.load(open(path_prefix + '/X_train.p', 'rb'))
+    X_test = pickle.load(open(path_prefix + '/X_test.p', 'rb'))
+    y_train = pickle.load(open(path_prefix + '/y_train.p', 'rb'))
+    y_test = pickle.load(open(path_prefix + '/y_test.p', 'rb'))
+    assert X_train.shape[1] == X_test.shape[1]
+    return X_train, X_test, y_train, y_test
+
 
 
 def precalculate_performance():
@@ -97,15 +111,7 @@ def precalculate_performance():
         df[c] = np.nan
     for idx, row in df.iterrows():
         print('Processing {} out of {}...'.format(idx, len(df)))
-        path_prefix = 'original_datasets/' + row['dataset_name']
-        try:
-            X_train = pickle.load(open(path_prefix + '/X_train.p', 'rb'))
-            X_test = pickle.load(open(path_prefix + '/X_test.p', 'rb'))
-            y_train = pickle.load(open(path_prefix + '/y_train.p', 'rb'))
-            y_test = pickle.load(open(path_prefix + '/y_test.p', 'rb'))
-            assert X_train.shape[1] == X_test.shape[1]
-        except FileNotFoundError:
-            print(path_prefix + ' not found')
+        X_train, X_test, y_train, y_test = _load_dataset('original_datasets/' + row['dataset_name'])
         trials = Trials()
         data = {'X_train': X_train, 'X_test': X_test, 'y_train': y_train, 'y_test': y_test}
         fmin_objective = partial(objective, data=data)
@@ -128,30 +134,47 @@ def _read_config(row: pd.Series) -> dict:
         'SVM_Linear_preprocessing', 'SVM_RBF_PCA', 'SVM_RBF_preprocessing', 'classifier_type', 'criterion',
         'max_features', 'min_samples_split'
     ]:
-        if not np.isnan(df.at[0, k]):
-            config[k] = int(df.at[0, k])
+        if not np.isnan(row[k]):
+            config[k] = int(row[k])
     return config
+
+
+def plot_validation_performance(dataset_name: str):
+    df = pickle.load(open('metafeatures_original_perf.p', 'rb'))
+    row = df[df['dataset_name'] == dataset_name]
+
+    # init_config = _read_config(row)
+    # init_vals = [init_config]
+    trials = Trials()
+    # trials = generate_trials_to_calculate(init_vals)
+    train_data = {
+        'X': pickle.load(open('original_datasets/' + dataset_name + '/X_train.p', 'rb')),
+        'y': pickle.load(open('original_datasets/' + dataset_name + '/y_train.p', 'rb')),
+    }
+    val_data = {
+        'X': pickle.load(open('original_datasets/' + dataset_name + '/X_test.p', 'rb')),
+        'y': pickle.load(open('original_datasets/' + dataset_name + '/y_test.p', 'rb')),
+    }
+    train_objective = partial(objective, data=train_data)
+    best = fmin(fn=train_objective, space=space, algo=tpe.suggest, max_evals=100, trials=trials)
+    space_eval(space, best)
+
+    # process trials
+    val_objective = partial(objective, data=val_data)
+    val_loss = []
+    for i, t in enumerate(trials.trials):
+        conf = space_eval(space, best)
+        val_loss.append(val_objective(conf))
+    plt.plot(range(len(val_loss)), val_loss)
+    plt.show()
+    _plot_best_so_far_loss(val_loss)
+
+
 
 
 if __name__ == '__main__':
     # precalculate_performance()
-    df = pickle.load(open('metafeatures_original_perf.p', 'rb')).iloc[0:10]
-    trials = Trials()
-
-    init_config = _read_config(df.iloc[0])
-    print(init_config)
-    init_vals = [init_config]
-
-    trials = generate_trials_to_calculate(init_vals)
-    data = {
-        'X_train': pickle.load(open('original_datasets/abalone/X_train.p', 'rb')),
-        'X_test': pickle.load(open('original_datasets/abalone/X_test.p', 'rb')),
-        'y_train': pickle.load(open('original_datasets/abalone/y_train.p', 'rb')),
-        'y_test': pickle.load(open('original_datasets/abalone/y_test.p', 'rb'))
-    }
-    fmin_objective = partial(objective, data=data)
-    best = fmin(fn=fmin_objective, space=space, algo=tpe.suggest, max_evals=3, trials=trials)
-    x_prime = space_eval(space, best)
-    print(x_prime)
+    #
+    plot_validation_performance('heart-h')
 
 
