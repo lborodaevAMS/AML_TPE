18e9192ce2560a34bd55864ab86c85e17a9f6f3f
leonidborodaev <sugarvio@gmail.com>
2020-12-22 08:11:41 +0100

    experiment 1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from hyperopt import hp, fmin, tpe, STATUS_OK, Trials, space_eval, rand
+from hyperopt import hp, fmin, tpe, Trials, space_eval, rand
 from hyperopt.fmin import generate_trials_to_calculate
-from sklearn import datasets
+from scipy.spatial import KDTree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC, LinearSVC
@@ -8,7 +8,6 @@ from sklearn.decomposition import PCA
 from sklearn.utils.testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.pipeline import Pipeline
-import os
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
@@ -89,19 +88,6 @@ def objective(args, data):
     return err
 
 
-def _plot_best_so_far_loss(losses):
-    y = np.minimum.accumulate(losses, axis=1)
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_xticks(np.arange(0, 20, 2))
-
-    plt.plot(range(y.shape[1]), y.mean(axis=0), markersize=5, c='darkgoldenrod', marker="s")
-
-    plt.fill_between(range(y.shape[1]), y.mean(axis=0) - y.std(axis=0), y.mean(axis=0) + y.std(axis=0), alpha=0.3, color='darkgoldenrod')
-
-
-    plt.show()
-
 
 def _load_dataset(path_prefix: str) -> typing.Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     X_train = pickle.load(open(path_prefix + '/X_train.p', 'rb'))
@@ -126,7 +112,7 @@ def precalculate_performance():
         trials = Trials()
         data = {'X_train': X_train, 'X_test': X_test, 'y_train': y_train, 'y_test': y_test}
         fmin_objective = partial(objective, data=data)
-        best = fmin(fn=fmin_objective, space=space, algo=rand.suggest, max_evals=50, trials=trials)
+        best = fmin(fn=fmin_objective, space=space, algo=rand.suggest, max_evals=40, trials=trials)
         for k, v in trials.best_trial['misc']['idxs'].items():
             if len(v) > 0:
                 df.at[idx, k] = v[0]
@@ -146,17 +132,13 @@ def _read_config(row: pd.Series) -> dict:
         'max_features', 'min_samples_split'
     ]:
         if not np.isnan(row[k]):
-            config[k] = int(row[k])
+            config[k] = row[k]
     return config
 
 
 def plot_validation_performance(dataset_name: str):
     df = pickle.load(open('metafeatures_original_perf.p', 'rb'))
-    row = df[df['dataset_name'] == dataset_name]
 
-    # init_config = _read_config(row)
-    # init_vals = [init_config]
-    # trials = generate_trials_to_calculate(init_vals)
     train_data = {
         'X': pickle.load(open('original_datasets/' + dataset_name + '/X_train.p', 'rb')),
         'y': pickle.load(open('original_datasets/' + dataset_name + '/y_train.p', 'rb')),
@@ -165,24 +147,66 @@ def plot_validation_performance(dataset_name: str):
         'X': pickle.load(open('original_datasets/' + dataset_name + '/X_test.p', 'rb')),
         'y': pickle.load(open('original_datasets/' + dataset_name + '/y_test.p', 'rb')),
     }
+
     train_objective = partial(objective, data=train_data)
     val_objective = partial(objective, data=val_data)
 
-    val_loss = np.zeros(shape=(5, 20))
+    # Evaluate without warm-starting
+    val_loss = np.zeros(shape=(10, 20))
+    # 5-fold CV
     for i in range(5):
         trials = Trials()
         fmin(fn=train_objective, space=space, algo=tpe.suggest, max_evals=20, trials=trials)
         for j in range(20):
             conf = __unpack_item_vals(trials.trials[0]["misc"]["vals"])
             magic_str = space_eval(space, conf)
-            # clf = __instantiate_classifier(magic_str)
             val_loss[i, j] = val_objective(magic_str)
 
-    _plot_best_so_far_loss(val_loss)
+    # Evaluate with warm-starring
+    init_vals = find_init_vals(dataset_name)
+    # 5-fold CV
+    for i in range(5, 10):
+        trials = generate_trials_to_calculate(init_vals)
+        fmin(fn=train_objective, space=space, algo=tpe.suggest, max_evals=15, trials=trials)
+        for j in range(20):
+            conf = __unpack_item_vals(trials.trials[j]["misc"]["vals"])
+            magic_str = space_eval(space, conf)
+            val_loss[i, j] = val_objective(magic_str)
+
+    y = np.minimum.accumulate(val_loss, axis=1)
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(111)
+    ax.set_xticks(np.arange(0, 20, 5))
+    ax.plot(range(y.shape[1]), y[0:5, :].mean(axis=0), markersize=5, c='darkgoldenrod', marker="s", label='TPE')
+    ax.fill_between(range(y.shape[1]), y[0:5, :].mean(axis=0) - y[0:5, :].std(axis=0), y[0:5, :].mean(axis=0) +
+                     y[0:5, :].std(axis=0), alpha=0.3, color='darkgoldenrod')
+
+    ax.plot(range(y.shape[1]), y[5:10, :].mean(axis=0), markersize=5, c='blue', marker="s")
+    ax.fill_between(range(y.shape[1]), y[5:10, :].mean(axis=0) - y[5:10, :].std(axis=0),
+                     y[5:10, :].mean(axis=0) + y[5:10, :].std(axis=0), alpha=0.3,
+                     color='blue', label='MI-TPE')
+    ax.set_xlabel('# function evaluations')
+    ax.set_ylabel('validation error')
+    ax.legend()
+    fig.savefig(dataset_name + '.png', dpi=300)
+    plt.show()
 
 
+def find_init_vals(dataset_name):
+    df = pickle.load(open('metafeatures_original_perf.p', 'rb'))
+    query = df[df['dataset_name'] == dataset_name].iloc[0, :-15].fillna(0)
+    df = df[df['dataset_name'] != dataset_name]
+    search_space = df[df['dataset_name'] != dataset_name].iloc[:, :-15].values
+    search_space = np.nan_to_num(search_space)
+    kdtree = KDTree(search_space)
+    d, idx = kdtree.query(query, 5)
+    init_vals = [_read_config(df.iloc[i, -14:]) for i in idx]
+    return init_vals
 
 
 if __name__ == '__main__':
-    plot_validation_performance('heart-h')
+    plot_validation_performance('primary-tumor')
+
+
+
 
